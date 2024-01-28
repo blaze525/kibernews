0e10d50dd8240bed78306947ade34c056ec6212c
rasulow <rasulov.olympusss@gmail.com>
2024-01-28 09:49:35 +0500

    commit

--- kibernews/urls.py
+++ kibernews/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
-from main.views import set_language
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
 
 
 main_app_patterns = [
@@ -17,6 +19,9 @@ main_app_patterns = i18n_patterns(
 urlpatterns = [
     path('admin/', admin.site.urls),
     *main_app_patterns,
-    
-    path('set_language/<str:language>', set_language, name='set-language')
+    path('set_language/<str:language>', views.set_language, name='set-language')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
